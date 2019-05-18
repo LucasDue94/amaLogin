1242ae342d55b777614c07c68c5f9e951dbf6d13
Magdiel Rocha <magdiel002@gmail.com>
2019-05-17 21:37:09 -0300

    Login 90% concluído

--- projeto-ama/src/login/login.js
+++ projeto-ama/src/login/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import logo from "../assets/logo.svg"
 
 const Main = styled.div`
     display:flex;
@@ -9,8 +10,9 @@ const Main = styled.div`
     width:100%;
     height:100%;
     background-color:#135F51;
+    color:white;
+    font-family:"Roboto", sans-serif;
 `;
-
 const SubContainer = styled.div`
     display:flex;
     flex-direction:column;
@@ -20,32 +22,71 @@ const SubContainer = styled.div`
     height:100%;
     background-color:#00876E;
 `;
-
 const LoginContainer = styled.div`
     display:flex;
-    flex-direction:column;
+    flex-direction: column;
     justify-content:center;
-    width:20em;
-    height:20em;
-    background-color:white;
+    align-items: center;
+    width: 25em;
+    height: 25em;
+    background-color: #135F51;
     border-radius: 5px;
+    box-shadow: 1px 1px 10px black;
 `;
 
+const Button = styled.button`
+    margin-top:10px;
+    color: white;
+    width: 66%;
+    padding:10px;
+    background-color: #CC2936;
+    border-radius:5px;
+    border:none;
+    cursor:pointer;
+    
+    ${this}:hover{
+        background-color:#DE767F;
+        transition:0.8s;
+    }
+`;
+const Input = styled.input`
+    width:'60%';
+    margin:'10px 0';
+    padding:'10px';
+    borderRadius: '5px';
+    border:'1px solid green';
+`;
+const LinkPassword = styled.a`
+    margin-top:10px;
+    text-decoration:none;
+    color: white;
+`;
+const logoStyle = {
+    width:'55%',
+    marginBottom:'20px',
+};
 const loginInput = {
-    color: 'red',
-    width: '50%'
+    width:'60%',
+    margin:'10px 0',
+    padding:'10px',
+    borderRadius: '5px',
+    border:'1px solid green',
 };
 
 function Login() {
-    return(
+    return (
         <Main>
             <SubContainer>
                 <LoginContainer>
-                    <input className={loginInput} type="text" placeholder={"E-mail ou nome do usuário"}/>
-                    <input type="text" placeholder={"Senha"}/>
+                <img style={logoStyle} src={logo} alt=""/>
+                    <input style={loginInput} type="text" placeholder={"Email ou nome do usuário"}/>
+                    <input style={loginInput} type="password" placeholder={"Senha"}/>
+                    <Button>Entrar</Button>
+                    <LinkPassword href="#">Esqueceu a senha?</LinkPassword>
                 </LoginContainer>
             </SubContainer>
         </Main>
     );
 }
+
 export default Login;
